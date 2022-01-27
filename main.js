6174856459402524d5600f51ccf01fff02ac8581
ShefkiBerisha <94741859+ShefkiBerisha@users.noreply.github.com>
2022-01-27 04:57:22 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+let btnOpen = document.QuerySelector('button');
+let site = ('https://support.savethechildren.org/site/Donation2?df_id=1620&1620.donation=form1'
+
+btnOpen.addEventListner('click', () => {
+  window.open(site)
+});
