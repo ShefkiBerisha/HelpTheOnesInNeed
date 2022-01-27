4988d55edd9e5c6b351c042bab5368673daa978d
ShefkiBerisha <94741859+ShefkiBerisha@users.noreply.github.com>
2022-01-27 16:03:10 +0100

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-let btnOpen = document.QuerySelector('button');
-let site = ('https://support.savethechildren.org/site/Donation2?df_id=1620&1620.donation=form1'
-
-btnOpen.addEventListner('click', () => {
-  window.open(site)
-});
